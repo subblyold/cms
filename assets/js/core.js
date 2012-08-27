2a8058c760dd2d322d254b03dcbb2c1989b331d0
Michael Lefebvre <michael@scenedata.com>
2012-08-27 21:30:32 +0200

    Main nave, desktop + iPad

---
+++ assets/js/core.js
@@ -0,0 +1,145 @@
+
+kickstarter.bootstrap('subbly');
+
+subbly.core = (function()
+{
+	var $global;
+
+	var sidebar = function()
+	{
+
+		var $sidebar      = $('#sidebar'),
+			$sidebarLinks = $sidebar.find('a');
+
+		if(!categorizr.isTablet())
+		{
+			var $subnav       = $sidebar.find('ul.sub-nav'),
+				$subnavActive = $sidebar.find('ul.sub-nav.open'),
+				$subnavLinks  = $subnav.find('a');
+
+			$sidebar.find('a.has-subnav').click(function(event)
+			{
+				event.preventDefault();
+				var $this = $(this);
+
+				$sidebarLinks.removeClass('active');
+
+				$this.addClass('active')
+
+				var $subnav = $this.next('ul');
+
+				if(!$subnav.hasClass('open'))
+				{
+					var $subnavActiveLinks = $subnavActive.find('li')
+
+					$subnavActiveLinks.removeClass('active');
+
+					$subnavActive.slideUp(200, function()
+					{
+						var $this = $(this);
+						$this.removeClass('open');
+						$subnavActiveLinks.removeClass('visible');
+					});
+					
+					$subnavActive = $subnav;
+
+					$subnav.slideDown(200, function()
+					{
+						$subnav.addClass('open');
+
+						$subnav.find('li').each(function(_index)
+						{
+							var delay = ((_index +1) * 100),
+								$this = $(this);
+
+							window.setTimeout(function()
+							{
+								$this.addClass('visible');					
+							}, delay)
+		 
+						});
+
+					});
+				}
+			});
+
+			$('#developper-tool-switch').find('a').click(function()
+			{
+				$(this).toggleClass('active');
+			});
+
+			var $userNav        = $('#user-nav'),
+				$userNavTrigger = $userNav.find('h3'),
+				$userNavList    = $userNav.find('ul'); 
+
+			$userNavTrigger.click(function()
+			{
+				var $this = $(this),
+					attr  = $this.attr('data-toggle');
+
+				if(attr == 'close')
+				{
+					$this.attr('data-toggle', 'open');
+
+					$userNav.outside(function()
+					{
+						$userNavTrigger.trigger('click');
+					})
+				}
+				else
+					$this.attr('data-toggle', 'close')
+				
+				$userNavList.slideToggle(200)
+			})
+		}
+		else
+		{
+			var $popover        = $('#subnav-popover'),
+				$popoverContent = $popover.find('ul');
+
+			$sidebar.find('a.has-subnav').click(function(event)
+			{
+				event.preventDefault();
+
+				var $this = $(this);
+
+				$sidebarLinks.removeClass('active');
+
+				$this.addClass('active')
+
+				var $subnav = $this.next('ul'),
+					offset = $this.offset().top;
+
+				$popoverContent.html($subnav.html());
+				$popover.css('top', offset).fadeIn(200, function()
+				{
+					$popover.outside(function(e)
+					{
+						$popover.fadeOut(200);
+					});
+				});
+
+
+
+			});
+		}
+	}
+
+	var finalize = function()
+	{
+		$('#global').find('.nano').nanoScroller({
+			classPane: 'track',
+			contentSelector: 'div.pane-content'
+		});
+	};
+
+	var init = function()
+	{
+		sidebar();
+	}
+
+	return {
+		'init': init,
+		'finalize': finalize
+	};
+})();
---
+++ assets/lib/nanoscroller/jquery.nanoscroller.js
@@ -0,0 +1,220 @@
+
+(function($, window, document) {
+  var DOMSCROLL, DOWN, DRAG, MOUSEDOWN, MOUSEMOVE, MOUSEUP, MOUSEWHEEL, NanoScroll, PANEDOWN, RESIZE, SCROLL, SCROLLBAR, UP, WHEEL, getScrollbarWidth, defaults, settings;
+  SCROLLBAR = 'scrollbar';
+  SCROLL = 'scroll';
+  MOUSEDOWN = 'mousedown';
+  MOUSEMOVE = 'mousemove';
+  MOUSEWHEEL = 'mousewheel';
+  MOUSEUP = 'mouseup';
+  RESIZE = 'resize';
+  DRAG = 'drag';
+  UP = 'up';
+  PANEDOWN = 'panedown';
+  DOMSCROLL = 'DOMMouseScroll';
+  DOWN = 'down';
+  WHEEL = 'wheel';
+  defaults = {
+    classPane: 'pane',
+    classSlider: 'slider',
+    contentSelector: 'div.content'
+  };
+  getScrollbarWidth = function() {
+    var noscrollWidth, outer, yesscrollWidth;
+    outer = document.createElement('div');
+    outer.style.position = 'absolute';
+    outer.style.width = '100px';
+    outer.style.height = '100px';
+    outer.style.overflow = 'scroll';
+    document.body.appendChild(outer);
+    noscrollWidth = outer.offsetWidth;
+    yesscrollWidth = outer.scrollWidth;
+    document.body.removeChild(outer);
+    return noscrollWidth - yesscrollWidth;
+  };
+  NanoScroll = (function() {
+
+    function NanoScroll(el) {
+      this.el = el;
+      this.generate();
+      this.createEvents();
+      this.addEvents();
+      this.reset();
+    }
+
+    NanoScroll.prototype.createEvents = function() {
+      var _this = this;
+      this.events = {
+        down: function(e) {
+          _this.isDrag = true;
+          _this.offsetY = e.clientY - _this.slider.offset().top;
+          _this.pane.addClass('active');
+          $(document).bind(MOUSEMOVE, _this.events[DRAG]);
+          $(document).bind(MOUSEUP, _this.events[UP]);
+          return false;
+        },
+        drag: function(e) {
+          _this.sliderY = e.clientY - _this.el.offset().top - _this.offsetY;
+          _this.scroll();
+          return false;
+        },
+        up: function(e) {
+          _this.isDrag = false;
+          _this.pane.removeClass('active');
+          $(document).unbind(MOUSEMOVE, _this.events[DRAG]);
+          $(document).unbind(MOUSEUP, _this.events[UP]);
+          return false;
+        },
+        resize: function(e) {
+          _this.reset();
+        },
+        panedown: function(e) {
+          _this.sliderY = e.clientY - _this.el.offset().top - _this.sliderH * 0.5;
+          _this.scroll();
+          _this.events.down(e);
+        },
+        scroll: function(e) {
+          var content, top;
+          content = _this.content[0];
+          if (_this.isDrag === true) return;
+          top = content.scrollTop / (content.scrollHeight - content.clientHeight) * (_this.paneH - _this.sliderH);
+          _this.slider.css({
+            top: top + 'px'
+          });
+        },
+        wheel: function(e) {
+          _this.sliderY += -e.wheelDeltaY || -e.delta;
+          _this.scroll();
+          return false;
+        }
+      };
+    };
+
+    NanoScroll.prototype.addEvents = function() {
+      var events, pane;
+      events = this.events;
+      pane = this.pane;
+      $(window).bind(RESIZE, events[RESIZE]);
+      this.slider.bind(MOUSEDOWN, events[DOWN]);
+      pane.bind(MOUSEDOWN, events[PANEDOWN]);
+      this.content.bind(SCROLL, events[SCROLL]);
+      if (window.addEventListener) {
+        pane = pane[0];
+        pane.addEventListener(MOUSEWHEEL, events[WHEEL], false);
+        pane.addEventListener(DOMSCROLL, events[WHEEL], false);
+      }
+    };
+
+    NanoScroll.prototype.removeEvents = function() {
+      var events, pane;
+      events = this.events;
+      pane = this.pane;
+      $(window).unbind(RESIZE, events[RESIZE]);
+      this.slider.unbind(MOUSEDOWN, events[DOWN]);
+      pane.unbind(MOUSEDOWN, events[PANEDOWN]);
+      this.content.unbind(SCROLL, events[SCROLL]);
+      if (window.addEventListener) {
+        pane = pane[0];
+        pane.removeEventListener(MOUSEWHEEL, events[WHEEL], false);
+        pane.removeEventListener(DOMSCROLL, events[WHEEL], false);
+      }
+    };
+
+    NanoScroll.prototype.generate = function() {
+      this.el.append('<div class="'+settings.classPane+'"><div class="'+settings.classSlider+'"></div></div>');
+      this.content =$(this.el.find(settings.contentSelector)[0]);
+      this.slider = this.el.find('.'+settings.classSlider);
+      this.pane = this.el.find('.'+settings.classPane);
+      this.scrollW = getScrollbarWidth();
+      if (this.scrollbarWidth === 0) this.scrollW = 0;
+      this.content.css({
+        right: -this.scrollW + 'px'
+      });
+    };
+
+    NanoScroll.prototype.reset = function() {
+      var content;
+      if (this.isDead === true) {
+        this.isDead = false;
+        this.pane.show();
+        this.addEvents();
+      }
+      content = this.content[0];
+      this.contentH = content.scrollHeight + this.scrollW;
+
+      var paneTop = parseInt(this.pane.css('top'));
+      var paneBottom = parseInt(this.pane.css('bottom'));
+
+      this.paneH = this.pane.outerHeight();
+      this.paneOuterH = this.paneH + paneTop + paneBottom;
+
+      this.sliderH = this.paneH / this.contentH * this.paneH;
+
+      this.sliderH = Math.round(this.sliderH);
+      this.scrollH = this.paneH - this.sliderH;
+      this.slider.height(this.sliderH);
+      this.diffH = content.scrollHeight - content.clientHeight;
+      this.pane.show();
+      if (this.paneOuterH >= this.content[0].scrollHeight) this.pane.hide();
+    };
+
+    NanoScroll.prototype.scroll = function() {
+      var scrollValue;
+      this.sliderY = Math.max(0, this.sliderY);
+      this.sliderY = Math.min(this.scrollH, this.sliderY);
+      scrollValue = this.paneH - this.contentH + this.scrollW;
+      scrollValue = scrollValue * this.sliderY / this.scrollH;
+      this.content.scrollTop(-scrollValue);
+      return this.slider.css({
+        top: this.sliderY
+      });
+    };
+
+    NanoScroll.prototype.scrollBottom = function(offsetY) {
+      var diffH, scrollTop;
+      diffH = this.diffH;
+      scrollTop = this.content[0].scrollTop;
+      this.reset();
+      if (scrollTop < diffH && scrollTop !== 0) return;
+      this.content.scrollTop(this.contentH - this.content.height() - offsetY);
+    };
+
+    NanoScroll.prototype.scrollTop = function(offsetY) {
+      this.reset();
+      this.content.scrollTop(offsetY + 0);
+    };
+
+    NanoScroll.prototype.stop = function() {
+      this.isDead = true;
+      this.removeEvents();
+      this.pane.hide();
+    };
+
+    return NanoScroll;
+
+  })();
+  $.fn.nanoScroller = function(options) {
+    var scrollbar;
+    settings = $.extend({}, defaults, options);
+
+    if (!($.browser.msie && $.browser.version < 7)) {
+        this.each(function() // allow multiple pane on page
+    {
+            var _this = $(this);
+            scrollbar = _this.data(SCROLLBAR);
+            if (scrollbar === void 0) {
+        scrollbar = new NanoScroll(_this);
+        _this.data(SCROLLBAR, scrollbar);
+            }
+            if (settings.scrollBottom) {
+                return scrollbar.scrollBottom(settings.scrollBottom);
+            }
+            if (settings.scrollTop) return scrollbar.scrollTop(settings.scrollTop);
+            if (settings.scroll === 'bottom') return scrollbar.scrollBottom(0);
+            if (settings.scroll === 'top') return scrollbar.scrollTop(0);
+            if (settings.stop) return scrollbar.stop();
+            scrollbar.reset();
+    });
+    }
+  };
+})(jQuery, window, document);
