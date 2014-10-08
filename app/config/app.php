c1cb74a413730af02b26592e1863458c6aa6ede0
Michael Lefebvre <michael@scenedata.com>
2014-10-08 16:50:47 +0200

    config sentry and debugbar

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 
@@ -188,7 +190,8 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-
+		'Sentry'            => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'Debugbar'          => 'Barryvdh\Debugbar\Facade',
 	),
 
 );
