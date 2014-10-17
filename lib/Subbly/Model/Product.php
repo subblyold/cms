3b70c5104d08f026197814632c5ff5d9718100b8
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-10-17 15:01:03 +0200

    Create Product model

---
+++ lib/Subbly/Model/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Subbly\Model;
+
+class OrderAddress extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+}
