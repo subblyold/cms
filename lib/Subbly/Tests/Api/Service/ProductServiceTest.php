95c3a39f1ac551d17a19f202f679da1ece40b38b
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-10-20 17:38:10 +0200

    Create ProductService

---
+++ lib/Subbly/Tests/Api/Service/ProductServiceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Subbly\Tests\Api\Service;
+
+use Subbly\Subbly;
+use Subbly\Api\Api;
+use Subbly\Api\Service\ProductService;
+use Subbly\Core\Container;
+
+class ProductServiceTest extends \PHPUnit_Framework_TestCase
+{
+    private function getService()
+    {
+        return Subbly::api('subbly.product');
+    }
+
+    public function testConstruct()
+    {
+        $api = new Api(new Container(), array());
+        $s   = new ProductService($api);
+
+        $this->assertNotNull($s);
+    }
+
+    public function testNewProduct()
+    {
+        $instance = $this->getService()->newProduct();
+        $this->assertInstanceOf('Subbly\\Model\\Product', $instance);
+    }
+
+    public function testName()
+    {
+        $this->assertEquals($this->getService()->name(), 'subbly.product');
+    }
+}
