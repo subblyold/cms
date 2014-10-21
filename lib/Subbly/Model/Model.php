fa7b7bafbbfc52384ec9940140cbbee8efeecb3b
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-10-21 14:06:37 +0200

    Optimize Subbly base model

--- lib/Subbly/Model/Model.php
+++ lib/Subbly/Model/Model.php
@@ -8,15 +8,43 @@ use Subbly\Api\Service\Service;
 
 abstract class Model extends Eloquent
 {
+    /**
+     *
+     */
     final public function save(array $options = array())
     {
+        $this->protectMethod($options);
+
+        return parent::save($options);
+    }
+
+    /**
+     *
+     */
+    final public function update(array $attributes = array())
+    {
+        $this->protectMethod($attributes);
+
+        return parent::update($attributes);
+    }
+
+    /**
+     * TODO protect some others methods like delete, push, ...
+     */
+
+    /**
+     *
+     */
+    private function protectMethod(array $options = array())
+    {
+        // var_dump($options);
         if (
-            isset($options['subbly_api_service'])
-            && $options['subbly_api_service'] instanceof Service
+            !isset($options['subbly_api_service'])
+            || !($options['subbly_api_service'] instanceof Service)
         ) {
             throw new \Exception('You must use an Subbly\Api\Service\Service to save a Model');
         }
 
-        return parent::save($options);
+        unset($options['subbly_api_service']);
     }
 }
