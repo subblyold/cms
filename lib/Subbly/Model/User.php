700020bca111d79e56b4073bfa8f16f31c34540b
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-10-22 14:26:30 +0200

    Clean User model

--- lib/Subbly/Model/User.php
+++ lib/Subbly/Model/User.php
@@ -2,15 +2,10 @@
 
 namespace Subbly\Model;
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Cartalyst\Sentry\Users\Eloquent\User as Model;
 
-class User extends Model implements UserInterface, RemindableInterface
+class User extends Model
 {
-    use UserTrait, RemindableTrait;
-
     /**
      * The database table used by the model.
      *
@@ -19,7 +14,7 @@ class User extends Model implements UserInterface, RemindableInterface
     protected $table = 'users';
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes visible from the model's JSON form.
      *
      * @var array
      */
