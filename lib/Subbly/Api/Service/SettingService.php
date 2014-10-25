c52b1fae292b318425ab3b26c393f6f172c0bebd
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-10-25 19:55:04 +0200

    Update Subbly serttings service

--- lib/Subbly/Api/Service/SettingService.php
+++ lib/Subbly/Api/Service/SettingService.php
@@ -3,7 +3,11 @@
 namespace Subbly\Api\Service;
 
 use Carbon\Carbon;
+
 use Illuminate\Support\Facades\Cache;
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 use Subbly\Model\Setting;
 
@@ -11,11 +15,14 @@ class SettingService extends Service
 {
     const CACHE_NAME = 'subbly.settings';
 
+    private $defaults = null;
+
     /**
      * Initialize the service
      */
     protected function init()
     {
+        $this->defaults = new \ArrayObject();
     }
 
     /**
@@ -23,14 +30,36 @@ class SettingService extends Service
      *
      * @param string  $filename Name of the file
      *
+     * @throws Subbly\Api\Service\Exception
+     * @throws Subbly\Api\Service\Exception
+     *
      * @api
      */
     public function registerDefaultSettings($filename)
     {
-        // TODO check if file exists
-        // TODO load yaml
+        // TODO Just register the filename and load the content after?
 
+        if (!file_exists($filename)) {
+            throw new Exception(sprintf('File for defaults settings with filename "%s" is not found', $filename));
+        }
 
+        try {
+            $yaml = Yaml::parse(file_get_contents($filename));
+        }
+        catch (ParseException $e) {
+            throw new Exception(sprintf('Unable to parse the YAML string: %s', $e->getMessage()));
+        }
+
+        if (!isset($yaml['default_settings']) && !is_array($yaml['default_settings'])) {
+            throw new Exception(sprintf('The defaults settings file "%s" must have "%s" key as root',
+                $filename,
+                'default_settings'
+            ));
+        }
+
+        $this->defaults = new \ArrayObject(
+            array_merge_recursive($this->defaults->getArrayCopy(), $yaml['default_settings'])
+        );
     }
 
     /**
@@ -52,6 +81,8 @@ class SettingService extends Service
      *
      * @return mixed
      *
+     * @throws Subbly\Api\Service\Exception
+     *
      * @api
      */
     public function get($key)
@@ -64,20 +95,46 @@ class SettingService extends Service
     }
 
     /**
+     * Ask if a setting key exists or not
      *
+     * @param string  $key  The setting key
+     *
+     * @return boolean
+     *
+     * @api
      */
     public function has($key)
     {
-        return $this->getCachedSettings()->offsetExists($key);
+        // TODO check the defaults key instead?
+        $exists = $this->getCachedSettings()->offsetExists($key);
+
+        /**
+         * Refresh cache if
+         */
+        if (!$exists)
+        {
+            $this->refresh();
+
+            $exists = $this->getCachedSettings()->offsetExists($key);
+        }
+
+        return $exists;
     }
 
     /**
+     * Update a setting value
      *
+     * @param string  $key   The setting key
+     * @param mixed   $value The setting value
+     *
+     * @api
      */
-    public function add($key, $value)
+    public function update($key, $value)
     {
         $this->fireEvent('adding', array($key, $value));
         // TODO check that identifier is defined into default_settings.yml
+
+        // TODO check the $value type in function of the default setting informations
 
         $settings = $this->getCachedSettings();
         // TODO identifier = PACKAGE_NAME.KEY_NAME
@@ -101,6 +158,42 @@ class SettingService extends Service
     }
 
     /**
+     * Get defaults settings
+     *
+     * @param string|null  $key A setting key (optional)
+     *
+     * @return array
+     *
+     * @api
+     */
+    public function defaults($key = null)
+    {
+        if (is_string($key))
+        {
+            if (!$this->defaults->offsetExists($key)) {
+                throw new Exception(sprintf(Exception::SETTING_KEY_NOT_EXISTS, $key));
+            }
+
+            return $this->defaults->offsetGet($key);
+        }
+
+        return $this->defaults->getArrayCopy();
+    }
+
+    /**
+     * Refresh the settings
+     *
+     * @param boolean  $force Force the refresh
+     *
+     * @api
+     */
+    public function refresh($force = false)
+    {
+        $this->initCachedSettings();
+    }
+
+    /**
+     * Get the cached settings data
      *
      * @return \ArrayObject
      */
@@ -117,6 +210,7 @@ class SettingService extends Service
     }
 
     /**
+     * Set new settings data to cache
      *
      * @param \ArrayObject  $settings
      */
@@ -137,6 +231,15 @@ class SettingService extends Service
     {
         $settings = new \ArrayObject;
 
+        // Defaults settings
+        foreach ($this->defaults as $k=>$v)
+        {
+            if (isset($v['value'])) {
+                $settings->offsetSet($k, $v['value']);
+            }
+        }
+
+        // Database settings
         foreach (Setting::all() as $s) {
             $settings->offsetSet($s->identifier, $s->value);
         }
