28c4e4d7f311109abb8945eb95fbac4a6bd39d6c
Michael Lefebvre <michael@scenedata.com>
2014-10-29 15:30:00 +0100

    backend route, wip
    problem with public directory files 403

--- app/routes.php
+++ app/routes.php
@@ -3,41 +3,13 @@
 /*
  * Register Backend routes before public routes.
  */
-App::before(function($request)
+Route::get( Config::get( 'subbly.backendUri', 'backend' ), function()
 {
-
-  /*
-   * Other pages
-   */
-  Route::group( [ 'prefix' => Config::get( 'subbly.backendUri', 'backend' ) ], function()
-  {
-    Route::any( '{slug}', 'TestController@showWelcome' );
-    // echo 'ok';
-    // Route::any( '{slug}', 'Backend\Classes\BackendController@run' )->where( 'slug', '(.*)?' );
-  });
-
-  /*
-   * Entry point
-   */
-  // Route::any( Config::get( 'subbly.backendUri', 'backend' ), 'Backend\Classes\BackendController@run' );
+  return View::make('backend');
 });
 
-
-Route::get('/test-option', 'TestOptionController@index');
-Route::post('/test-option', 'TestOptionController@save');
-
-/*
- * You can set you own route bellow
- * see Laravel's doc form more information:
- * http://laravel.com/docs/4.2/routing
- */
-
-// Route::get('/your-route', 'YourRoute@yourMethod');
-
-
-
 /**
- * Backend routes
+ * Backend proxy API routes
  */
 Route::group(array(
     'prefix'    => Config::get( 'subbly.apiUri', 'api' ),
