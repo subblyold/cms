83db9481b658dd98d792d5e552811ee16f23acd4
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-11-03 17:21:21 +0100

    Update Order model

--- lib/Subbly/Model/Order.php
+++ lib/Subbly/Model/Order.php
@@ -16,6 +16,29 @@ class Order extends Model implements ModelInterface
     protected $table = 'orders';
 
     /**
+     * The attributes visible from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $visible = array('id', 'status', 'total_price', 'shipping_address', 'billing_address', 'products', 'user');
+
+    protected $fillable = array('status', 'user_id', 'shipping_address', 'billing_address');
+
+    protected $defaultValues = array(
+        'status'      => self::STATUS_DRAFT,
+        'total_price' => 0,
+    );
+
+    const STATUS_DRAFT = 'draft';
+
+    /**
+     * Validations
+     */
+    protected $rules = array(
+        'user_id' => 'required',
+    );
+
+    /**
      * Relashionship
      */
     public function user()
@@ -23,17 +46,17 @@ class Order extends Model implements ModelInterface
         return $this->belongsTo('Subbly\\Model\\User', 'user_id');
     }
 
-    public function product()
+    public function products()
     {
         return $this->hasMany('Subbly\\Model\\OrderProduct');
     }
 
-    public function shippingAddress()
+    public function shipping_address()
     {
         return $this->belongsTo('Subbly\\Model\\OrderAddress', 'shipping_address_id');
     }
 
-    public function billingAddress()
+    public function billing_address()
     {
         return $this->belongsTo('Subbly\\Model\\OrderAddress', 'billing_address_id');
     }
