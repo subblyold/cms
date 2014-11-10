5fee4490d6e02a20e302f76c6e812fe1300f6a89
michael-lefebvre <michael@scenedata.com>
2014-11-11 00:42:45 +0100

    CUI, customers tpl

--- src/js/view.js
+++ src/js/view.js
@@ -5,6 +5,7 @@ var SubblyView = Backbone.View.extend(
 {
     _viewId:   false
   , _viewName: false
+  , _viewTpl:  false
 
   , initialize: function( options )
     {
@@ -12,6 +13,19 @@ var SubblyView = Backbone.View.extend(
 
       // console.log(' initialize ' + this._viewName, this._viewId, this.el)
 
+      this.setHtml()
+
       return this
+    }
+
+  , setHtml: function()
+    {
+      if( !this._viewTpl )
+        return
+
+      var tpl  = Handlebars.compile( this._viewTpl )
+        , html = tpl( {} )
+
+      this.$el.html( html )
     }
 })
