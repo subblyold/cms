e08c4e8f175747f411edf8d8cd92785e1a149486
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-11-11 00:37:12 +0100

    Update UserAddress model

--- lib/Subbly/Model/UserAddress.php
+++ lib/Subbly/Model/UserAddress.php
@@ -9,13 +9,40 @@ class UserAddress extends Model implements ModelInterface
     use Concerns\SubblyModel;
     use Concerns\AddressTrait;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
     protected $table = 'user_addresses';
 
+    /**
+     * Fields
+     */
+    protected $visible = array('uid', 'firstname', 'lastname', 'address1', 'address2', 'zipcode', 'city', 'country', 'created_at', 'updated_at');
+
+    protected $fillable = array('firstname', 'lastname');
+
+    /**
+     * Validations
+     */
+    protected $rules = array(
+        'firstname' => 'required',
+        'lastname'  => 'required',
+        'address1'  => 'required',
+        'zipcode'   => 'required',
+        'city'      => 'required',
+        'country'   => 'required',
+    );
+
+    /**
+     *
+     */
+    protected function performInsert(\Illuminate\Database\Eloquent\Builder $query, array $options)
+    {
+        $this->attributes['uid'] = md5(uniqid(mt_rand(), true));
+
+        parent::performInsert($query, $options);
+    }
+
+    /**
+     * Relationships
+     */
     public function user()
     {
         return $this->belongsTo('Subbly\\Model\\User');
