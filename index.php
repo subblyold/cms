3ebd7e60a69e5ef60de43e37b9a8d72542929963
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-11-13 02:32:25 +0100

    Update and add base files

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Subbly - The PHP e-commerce that gets back to basics.
  *
  * @package  Subbly
- * @author   Benjamin Guedj, Michael Lefebvre, Sebastien Penet
+ * @author   Benjamin Guedj, Michael Lefebvre, Nicolas Brousse
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
