8bb635d19653a5853ad44f9038cfde2f82c873af
michael-lefebvre <michael@scenedata.com>
2014-12-11 02:05:29 +0100

    Upload Helper

--- bootstrap/tools.php
+++ bootstrap/tools.php
@@ -28,3 +28,36 @@ if ( ! function_exists('public_upload'))
     // return app()->make('path.upload').($path ? '/'.$path : $path);
   }
 }
+
+if ( ! function_exists('convertBytes'))
+{
+  // http://stackoverflow.com/a/13223499
+  function convertBytes( $value ) 
+  {
+    if ( is_numeric( $value ) ) 
+    {
+      return $value;
+    }
+    else
+    {
+      $value_length = strlen($value);
+      $qty          = substr( $value, 0, $value_length - 1 );
+      $unit         = strtolower( substr( $value, $value_length - 1 ) );
+
+      switch( $unit ) 
+      {
+        case 'k':
+          $qty *= 1024;
+          break;
+        case 'm':
+          $qty *= 1048576;
+          break;
+        case 'g':
+          $qty *= 1073741824;
+          break;
+      }
+
+      return $qty;
+    }
+  }
+}
