45915ee18af6dd1ce73a1cafb7ef69d4cffaa850
michael-lefebvre <michael@scenedata.com>
2014-12-15 01:05:28 +0100

    App defaults locales

--- app/config/subbly.php
+++ app/config/subbly.php
@@ -26,6 +26,32 @@ return array(
 
     /*
     |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
+
+  , 'frontendLocales' => array('en', 'fr', 'mx')
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Fallback Locale
+    |--------------------------------------------------------------------------
+    |
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
+    |
+    */
+
+  , 'frontendFallbackLocale' => 'en'
+
+    /*
+    |--------------------------------------------------------------------------
     | Front-end URI list
     |--------------------------------------------------------------------------
     |
