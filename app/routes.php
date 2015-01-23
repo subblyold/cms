e01004a6679f7ce41ae1ac712a18f0a113c891f3
michael-lefebvre <michael@scenedata.com>
2015-01-23 20:37:27 +0100

    clean route

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,6 @@
 <?php
 
 /*
- * Register template driven Frontend
- * Comment this part if you want to user
- * your own controller
- */
-
-//Route::any('{url}', 'AutoController@run')->where('url', '.*');
-
-/*
  * You can set you own route bellow
  * see Laravel's doc form more information:
  * http://laravel.com/docs/4.2/routing
@@ -16,5 +8,3 @@
 
 // Route::get('/your-route', 'YourRoute@yourMethod');
 
-// $test = new AutoController;
-// dd( $test );
