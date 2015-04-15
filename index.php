0fd5a9d72bf18e0963091a6fe6856adcd5d7212d
Nicolas Brousse <pro@nicolas-brousse.fr>
2015-04-15 20:56:27 +0200

    Update the index file to load the Subbly application

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
  * @package  Subbly
  * @author   Benjamin Guedj, Michael Lefebvre, Nicolas Brousse
  */
+define('LARAVEL_START', microtime(true));
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new \Subbly\Application();
+Patchwork\Utf8\Bootup::initMbstring();
+Illuminate\Support\ClassLoader::register();
+
+$app = new \Subbly\Framework\Application();
+$app->setRootDirectory(__DIR__);
+$app->setConfigDirectory(__DIR__ . '/config/');
+$app->start();
 $app->run();
